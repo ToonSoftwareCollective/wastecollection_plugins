e1e4750495f718b206ab7c314b830577b9ca7148
ToonzDev <35101966+ToonzDev@users.noreply.github.com>
2021-12-31 15:30:40 +0100

    initial release

---
+++ wastecollectionProvider_0.js
@@ -0,0 +1,50 @@
+//<provider>0</provider><version>1.0.0</version><parms>"none"</parms>
+//just manually provided extra dates
+
+	function readCalendar(wasteZipcode, wasteHouseNr, extraDates, enableCreateICS, wasteICSId, wasteStreet, wasteStreetName, wasteCity, wasteFullICSUrl) {
+	
+		// do nothing
+
+	}
+
+	function sortArray2(inputarray, extraDates) {
+	
+			var newArray = inputarray.concat(extraDates);
+			newArray.sort();
+
+			return newArray;
+	}
+
+	function writeWasteDates(wasteDatesString, enableCreateICS) {
+	
+   		var doc2 = new XMLHttpRequest();
+   		doc2.open("PUT", "file:///var/volatile/tmp/wasteDates.txt");
+		doc2.onreadystatechange=function() {
+			if (doc2.readyState === 4){
+				if (doc2.status === 0) {
+					updateWasteIcon("no");
+				}
+			}
+		}
+   		doc2.send(wasteDatesString);
+
+		// create ICS file for use in the calendar app when requested
+
+		if (enableCreateICS) {
+			var outputICS = "";
+			var tmpICS = wasteDatesString.split("\n");
+
+			for (var i = 0; i < tmpICS.length; i++) {
+				if (tmpICS[i].length > 10) { 
+					outputICS = outputICS + "BEGIN:VEVENT\r\n";
+					outputICS = outputICS + "DTSTART;VALUE=DATE:" + tmpICS[i].substring(0,4) + tmpICS[i].substring(5,7) + tmpICS[i].substring(8,10) + "\r\n";
+					outputICS = outputICS + "SUMMARY:" + wasteTypeFriendlyName(tmpICS[i].substring(11,12)) + "\r\n";
+					outputICS = outputICS + "BEGIN:VEVENT\r\n";
+				}
+			}
+   			var doc3 = new XMLHttpRequest();
+   			doc3.open("PUT", "file:///var/volatile/tmp/wasteDates.ics");
+   			doc3.send(outputICS);
+
+		}
+	}
