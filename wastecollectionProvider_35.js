86eef41bb04d1793c7366fbdc0dd335d10f96348
ToonzDev <35101966+ToonzDev@users.noreply.github.com>
2025-10-29 12:38:16 +0100

    Updated Reinis.nl

--- wastecollectionProvider_35.js
+++ wastecollectionProvider_35.js
@@ -1,89 +1,60 @@
-//<provider>35</provider><version>1.0.0</version><parms>"zipcode,housenr"</parms>
+//<provider>35</provider><version>1.0.1</version><parms>"zipcode,housenr"</parms>
 //provider reinis.nl testdata: 3206NG 22
 
 	function readCalendar(wasteZipcode, wasteHouseNr, extraDates, enableCreateICS, wasteICSId, wasteStreet, wasteStreetName, wasteCity, wasteFullICSUrl) {
 
-       		var params = "companyCode=9dc25c8a-175a-4a41-b7a1-83f237a80b77&postCode=" + wasteZipcode + "&houseNumber=" + wasteHouseNr + "&houseLetter=&houseNumberAddition=";
+		var i = 0;
+		var j = 0;
+		var k = 0;
+		var wasteDatesString = "";
+		var wasteType = "";
+		var wasteDatesArray = [];
 		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.open("POST", "https://wasteapi.2go-mobile.com/api/FetchAdress", true);
-        	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        	xmlhttp.setRequestHeader("Content-length", params.length);
-        	xmlhttp.setRequestHeader("Connection", "close");
-		xmlhttp.onreadystatechange = function() {
-			if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-				addressJson = JSON.parse(xmlhttp.responseText);
-				read2goMobileCalendar(addressJson['dataList'][0]['UniqueId']);
-			}
-		}
-		xmlhttp.send(params);
-	}
 
-	function read2goMobileCalendar(uniqueId) {
+		xmlhttp.onreadystatechange=function() {
+			if (xmlhttp.readyState == 4) {
+				if (xmlhttp.status == 200) {
+					var aNode = xmlhttp.responseText;
 
-		var now = new Date;
-		var strMonNow = now.getMonth() + 1;
-		if (strMonNow < 10) {
-			strMonNow = "0" + strMonNow;
-		}
-		var strDayNow = now.getDate();
-		if (strDayNow < 10) {
-			strDayNow = "0" + strDayNow;
-		}
+					// read specific waste collection dates check DATE format
 
-		var later = new Date(now.getTime() + 7772000000); // 3 months later
-		var strMonLater = later.getMonth() + 1;
-		if (strMonLater < 10) {
-			strMonLater = "0" + strMonLater;
-		}
-		var strDayLater = later.getDate();
-		if (strDayLater < 10) {
-			strDayLater = "0" + strDayLater;
-		}
-
-		var startDate = now.getFullYear() + "-" + strMonNow + "-" +  strDayNow; 
-		var endDate = later.getFullYear() + "-" + strMonLater + "-" +  strDayLater;
-		var params = "companyCode=9dc25c8a-175a-4a41-b7a1-83f237a80b77&uniqueAddressId=" + uniqueId + "&startDate=" + startDate + "&endDate=" + endDate;
-
-		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.open("POST", "https://wasteapi.2go-mobile.com/api/GetCalendar", true);
-        	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        	xmlhttp.setRequestHeader("Content-length", params.length);
-        	xmlhttp.setRequestHeader("Connection", "close");
-		xmlhttp.onreadystatechange = function() {
-			if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-				wasteDatesString = "";
-				var wasteType = "";
-				var mobileAfvalbeheerDates = [];
-				var calendar2goMobile = JSON.parse(xmlhttp.responseText)
-				for (var i= 0; i < calendar2goMobile['dataList'].length; i++) {
-					for (var j= 0; j < calendar2goMobile['dataList'][i]['pickupDates'].length; j++) {
-						if (calendar2goMobile['dataList'][i]['_pickupTypeText'] == "GREENGREY") {
-							mobileAfvalbeheerDates.push(calendar2goMobile['dataList'][i]['pickupDates'][j].substring(0,10) + "-" + wasteType2goMobile("GREEN"));
-							mobileAfvalbeheerDates.push(calendar2goMobile['dataList'][i]['pickupDates'][j].substring(0,10) + "-" + wasteType2goMobile("GREY"));
-						} else {
-							mobileAfvalbeheerDates.push(calendar2goMobile['dataList'][i]['pickupDates'][j].substring(0,10) + "-" + wasteType2goMobile(calendar2goMobile['dataList'][i]['_pickupTypeText']));
-						}
+					i = aNode.indexOf("DTSTART");
+					if (i > 0) {
+						if (aNode.substring(i, i+18) == "DTSTART;VALUE=DATE") i = i + 11;
 					}
-				}
-				var tmp = sortArray2(mobileAfvalbeheerDates, extraDates);
 
+					if ( i > 0 ) {
+						while (i > 0) {
+							j = aNode.indexOf("SUMMARY", i);
+
+							wasteType = wasteTypeCode(aNode.substring(j+8, j+14));
+							wasteDatesArray.push(aNode.substring(i+8, i+12) + "-" + aNode.substring(i+12, i+14) + "-" + aNode.substring(i+14, i+16) + "," + wasteType);
+
+							i = aNode.indexOf("DTSTART", i + 10);	
+							if (i > 0) {
+								if (aNode.substring(i, i+18) == "DTSTART;VALUE=DATE") i = i + 11;
+							}
+						}
+					}	
+				}
+				var tmp = sortArray2(wasteDatesArray, extraDates);
 				for (i = 0; i < tmp.length; i++) {
 					wasteDatesString = wasteDatesString + tmp[i] + "\n";
 				}
 				writeWasteDates(wasteDatesString, enableCreateICS);
 			}
-		}
-		xmlhttp.send(params);
+		} 
+		xmlhttp.open("GET", "https://reinis.nl/ical/" + wasteICSId, true);
+		xmlhttp.send();
 	}
 
-	function wasteType2goMobile(shortName) {
+
+	function wasteTypeCode(shortName) {
 		switch (shortName) {
-			case "GREEN": return 3;		//groente/fruit	
-			case "PAPER": return 2;		//papier
-			case "PACKAGES": return 1;	//pmd
-			case "PLASTIC": return 1;	//pmd
-			case "VET": return 7;		//KGA meppel.nl
-			case "GREY": return 0;		//restafval
+			case "Groent": return 3;	//groente/fruit	
+			case "Oud Pa": return 2;	//papier
+			case "Plasti": return 1;	//pmd
+			case "Restaf": return 0;		//restafval
 			default: break;
 		}
 		return "?";
